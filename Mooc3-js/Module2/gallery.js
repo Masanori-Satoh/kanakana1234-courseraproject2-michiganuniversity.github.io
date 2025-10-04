97ddc0ba5b7bff4446240b95e365ac31befa7746
Masanori-Satoh <duckgamer2k6@gmail.com>
2025-10-04 14:10:35 +0700

    Add files via upload

---
+++ Mooc3-js/Module2/gallery.js
@@ -0,0 +1,12 @@
+/* gallery.js */
+function upDate(previewPic){
+  const display = document.getElementById("image");
+  display.style.backgroundImage = `url("${previewPic.src}")`;
+  display.textContent = previewPic.alt;
+}
+
+function unDo(){
+  const display = document.getElementById("image");
+  display.style.backgroundImage = ""; // trở về giá trị gốc (không có ảnh)
+  display.textContent = "Hover over an image below to display here.";
+}
